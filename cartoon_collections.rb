bb122b030db20e32b27d46fcd27c93a5d948fc7d
Jonathan Reid <iamjonathanreid@gmail.com>
2020-06-02 05:24:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  
   array.each do |element|
     puts "Howdy #{element}!"
   end
